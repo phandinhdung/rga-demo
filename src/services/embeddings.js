221d64c15e6d567d4d772433edaf5cf41b540cf3
dungcv <phandinhdungcv@gmail.com>
2026-01-10 13:30:16 +0700

    Thêm maxConcurrency vào OllamaEmbeddings và cập nhật phương thức tìm kiếm trong Pinecone retriever

--- src/services/embeddings.js
+++ src/services/embeddings.js
@@ -4,6 +4,7 @@ import { OllamaEmbeddings } from "@langchain/ollama";
 export const embeddings = new OllamaEmbeddings({
   model: process.env.EMBEDDING_MODEL_NAME,
   baseUrl: process.env.OLLAMA_BASE_URL,
+  maxConcurrency: 3
 });
 
 console.log(`--- Đã khởi tạo Embedding Model: ${process.env.EMBEDDING_MODEL_NAME} ---`);
